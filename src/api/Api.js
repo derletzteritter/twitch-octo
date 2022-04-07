6afd45eb1d13c47b8e5eced0047537d04d9f64d2
Christopher <chip@projecterror.dev>
2022-04-07 16:44:08 +0200

    Update Api.js

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 let api = axios.create({
   headers: {
     'Content-Type': 'application/json',
-    'Client-ID': 'cf3x18skxfvchtla7le430orertml3',
-    'Authorization': 'Bearer 6kidv178bkso22m1m1khzu6a7xgwbt'
+    'Client-ID': '',
+    'Authorization': 'Bearer'
   }
 })
 export default api;
